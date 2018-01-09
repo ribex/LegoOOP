a7c7c3d718bb990a0da0069bdba8bc18350536fe
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-08 20:16:48 -0500

    Implement constructor

--- Bulldozer/Bulldozer/Program.cs
+++ Bulldozer/Bulldozer/Program.cs
@@ -6,19 +6,23 @@ namespace Bulldozer
     {
         static void Main(string[] args)
         {
-            var dozer = new BullDozer();
-            dozer.color = "Yellow";
-            dozer.size = "Mini";
-            var dozer2 = new BullDozer();
-            dozer2.color = "Red";
-            dozer2.size = "Large";
+            var dozer1 = new BullDozer("Yellow", "Mini");
+            var dozer2 = new BullDozer("Red", "Large");
 
-            Console.WriteLine("Bulldozer is: " + dozer.color);
+
+            Console.WriteLine("Bulldozer 1 is: " + dozer1.color);
+            Console.WriteLine("Bulldozer 2 is: " + dozer2.color);
         }
     }
 
     public class BullDozer
     {
+        public BullDozer (string _color, string _size)
+        {
+            this.color = _color;
+            this.size = _size;
+        }
+
         public string color { get; set; }
         public string size { get; set; }
     }
