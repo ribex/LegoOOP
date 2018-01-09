fcd5c675574d4fa3880385fe2582924e1af43763
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-08 20:13:17 -0500

    Add BullDozer class

---
+++ Bulldozer/Bulldozer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bulldozer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dozer = new BullDozer();
+            dozer.color = "Yellow";
+            dozer.size = "Mini";
+            var dozer2 = new BullDozer();
+            dozer2.color = "Red";
+            dozer2.size = "Large";
+
+            Console.WriteLine("Bulldozer is: " + dozer.color);
+        }
+    }
+
+    public class BullDozer
+    {
+        public string color { get; set; }
+        public string size { get; set; }
+    }
+}
